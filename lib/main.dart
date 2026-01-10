bb60efef488663da17ccf0aedbbf56559b498aa3
Austin Bray <93416177+AustinBray77@users.noreply.github.com>
2026-01-10 15:55:18 -0500

    Updates to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mac_battles/db.dart';
+import 'package:mac_battles/homepage.dart';
 import 'package:mac_battles/types.dart';
 
 Future<void> main() async {
@@ -8,52 +9,21 @@ Future<void> main() async {
 
   await dotenv.load(fileName: ".env");
   await initializeDB();
+  const title = 'Mac Battles';
 
-  runApp(MyApp());
+  runApp(MyApp(title: title));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    const primary = Color(0xFF764248);
-    const surface = Color(0xFFF6F7EB);
-    const secondary = Color(0xFFE28413);
-    const highlight = Color(0xFF6369D1);
-    const dark = Color(0xFF333232);
-
-    return MaterialApp(
-      title: 'Mac Battles',
-      theme: ThemeData(
-        colorScheme: ColorScheme(
-          brightness: Brightness.light,
-          primary: primary,
-          onPrimary: surface,
-          secondary: secondary,
-          onSecondary: dark,
-          error: secondary,
-          onError: dark,
-          surface: surface,
-          onSurface: dark,
-        ),
-      ),
-      home: const MyHomePage(title: 'Mac Battles'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyApp> createState() => _MyApp();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyApp extends State<MyApp> {
   AppState? _state;
 
   void _loadState() async {
@@ -62,19 +32,54 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    if (_state == null) {
-      _loadState();
-      return const Scaffold(body: Center(child: CircularProgressIndicator()));
-    }
-
+  Widget inAppScreen(BuildContext context, Widget w) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.surface,
         title: Text(widget.title),
       ),
-      body: Center(),
+      body: Center(child: w),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const primary = Color(0xFF764248);
+    const surface = Color(0xFFF6F7EB);
+    const secondary = Color(0xFFE28413);
+    const highlight = Color(0xFF6369D1);
+    const dark = Color(0xFF333232);
+
+    var theme = ThemeData(
+      colorScheme: ColorScheme(
+        brightness: Brightness.light,
+        primary: surface,
+        onPrimary: primary,
+        secondary: secondary,
+        onSecondary: dark,
+        error: secondary,
+        onError: dark,
+        surface: surface,
+        onSurface: dark,
+      ),
+    );
+
+    if (_state == null) {
+      _loadState();
+      return MaterialApp(
+        title: widget.title,
+        theme: theme,
+        home: Scaffold(body: Center(child: CircularProgressIndicator())),
+      );
+    }
+
+    return MaterialApp(
+      title: widget.title,
+      theme: theme,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => inAppScreen(context, HomePage(user: _state!.user)),
+      },
     );
   }
 }
